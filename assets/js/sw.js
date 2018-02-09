bbe1581081c5e2a38ca5f355bf473025254a60ce
Oleg Mordach <omordach@gmail.com>
2018-02-09 20:32:37 +0200

    Another changes to PWA features

---
+++ assets/js/sw.js
@@ -0,0 +1,4 @@
+"use strict";
+importScripts("sw-toolbox.js");
+toolbox.precache(["home.html","assets/*", "images/*"]);
+toolbox.router.get("/images/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
